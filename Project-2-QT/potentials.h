c6656d6fbe644933c10d7e1f852fb48c7570ebca
olegjo <olegunnar@1x-193-157-221-208.uio.no>
2015-10-02 16:17:56 +0200

    Better organization, added unit tests

--- Project-2-QT/potentials.h
+++
@@ -1,5 +0,0 @@
-namespace Potentials {
-    void oneElectron(double **A, int n, double rho_min, double h);
-    void twoElectrons(double **A, int n, double rho_min, double h, double omega_r);
-}
-
--- jacobi-method.h
+++
@@ -1,3 +0,0 @@
-double max_off_diag(double **A, int *l, int *k, int n);
-void rotate(double **A, int k, int l, int n);
-void jacobi_method(double **A, int n);
